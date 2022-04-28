f0c66f73f5b216ab1bf8439f27fd7ff0712a8927
bequ1n <kulikovskikh@bk.ru>
2022-04-28 23:44:55 +0300

    add pwntools.cpp

---
+++ pwntools.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#include "arif.cpp"
+
+using namespace std;
+
+int main()
+{
+    
+}
